fd38d387b0e230c31585c44cad97aa7ddaebd729
Mark Williams <mark.williams@enable.com>
2022-12-01 08:06:47 +0000

    1.2

--- aoc-2022-rust/src/main.rs
+++ aoc-2022-rust/src/main.rs
@@ -3,7 +3,8 @@ use file_helpers::{get_int_chunks_from_file, get_int_lines_from_file, get_lines_
 mod file_helpers;
 
 fn main() {
-    day_1_part_1()
+    day_1_part_1();
+    day_1_part_2();
 }
 
 pub fn day_1_part_1() {
@@ -11,4 +12,15 @@ pub fn day_1_part_1() {
 
     let elf_totals: Vec<i32> = elves.iter().map(|elf| elf.iter().sum::<i32>()).collect();
     println!("{:?}", elf_totals.iter().max());
+}
+
+pub fn day_1_part_2() {
+    let elves = get_int_chunks_from_file("../inputs/1.txt");
+
+    let mut elf_totals: Vec<i32> = elves.iter().map(|elf| elf.iter().sum::<i32>()).collect();
+    elf_totals.sort();
+    elf_totals.reverse();
+
+    let top_3_total = elf_totals[0] + elf_totals[1] + elf_totals[2];
+    println!("{:?}", top_3_total)
 }
