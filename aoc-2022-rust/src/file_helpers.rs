bd4d8e95f7bbd156cf5b4bc420bc0604127600e2
Mark Williams <mark.williams@enable.com>
2022-12-01 07:54:40 +0000

    WIP

--- aoc-2022-rust/src/file_helpers.rs
+++ aoc-2022-rust/src/file_helpers.rs
@@ -19,3 +19,26 @@ pub fn get_int_lines_from_file(filename: impl AsRef<Path>) -> Vec<i32> {
         .map(|l| str::parse::<i32>(&l).expect("Could not parse to int"))
         .collect()
 }
+
+pub fn get_int_chunks_from_file(filename: impl AsRef<Path>) -> Vec<Vec<i32>> {
+    let all_lines = get_lines_from_file(filename);
+    let mut result: Vec<Vec<i32>> = vec![];
+    let mut current_chunk: Vec<i32> = vec![];
+
+    for line in all_lines {
+        if line.is_empty() {
+            print!("new chunk");
+            let clone = current_chunk.clone();
+            result.push(clone);
+            current_chunk = vec![];
+        } else {
+            current_chunk.push(str::parse::<i32>(&line).expect("Could not parse to int"));
+        }
+    }
+
+    if current_chunk.len() > 0 {
+        result.push(current_chunk);
+    }
+
+    result
+}
--- aoc-2022-rust/src/main.rs
+++ aoc-2022-rust/src/main.rs
@@ -1,15 +1,18 @@
-use file_helpers::{get_int_lines_from_file, get_lines_from_file};
+use file_helpers::{get_int_chunks_from_file, get_int_lines_from_file, get_lines_from_file};
 
 mod file_helpers;
 
 fn main() {
-    let lines = get_lines_from_file("../inputs/words.txt");
-    for line in lines {
-        println!("{:?}", line);
-    }
+    day_1_part_1()
+}
 
-    let lines = get_int_lines_from_file("../inputs/numbers.txt");
-    for line in lines {
-        println!("{:?}", line);
+pub fn day_1_part_1() {
+    let chunks = get_int_chunks_from_file("../inputs/1.txt");
+    for chunk in &chunks {
+        for line in chunk {
+            print!("{line} ")
+        }
+        println!("\n");
     }
+    println!("{}", chunks.len());
 }
