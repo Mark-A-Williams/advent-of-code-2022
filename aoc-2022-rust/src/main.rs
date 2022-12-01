cb20b392843f52757d026ceacdeb3a0bdd5d4a81
Mark Williams <amwilliams137@gmail.com>
2022-12-01 23:47:21 +0000

    \n\n

--- aoc-2022-rust/src/main.rs
+++ aoc-2022-rust/src/main.rs
@@ -8,7 +8,9 @@ fn main() {
     let mut sw = Stopwatch::start_new();
     part_1();
     println!("Part 1 completed in {}ms", sw.elapsed_ms());
+
     sw.reset();
+    
     sw.start();
     part_2();
     println!("Part 2 completed in {}ms", sw.elapsed_ms());
