c3f7788e5f13f0b1b85560e03710bea1fd8b01f5
Mark Williams <mark.williams@enable.com>
2022-12-02 21:23:39 +0000

    2.2

--- aoc-2022-rust/src/solutions/day_2.rs
+++ aoc-2022-rust/src/solutions/day_2.rs
@@ -6,43 +6,86 @@ pub fn part_1() {
 
     for round in rounds {
         let parsed: Vec<&str> = round.split(' ').collect();
-        let opponent_choice = parsed[0];
-        let my_choice = parsed[1];
+        let opponent_choice = parse_opponent_play(parsed[0]);
 
-        match my_choice {
-            "X" => score += 1,
-            "Y" => score += 2,
-            "Z" => score += 3,
-            _ => panic!("Unexpected choices"),
-        }
+        let my_choice = match parsed[1] {
+            "X" => Choice::Rock,
+            "Y" => Choice::Paper,
+            "Z" => Choice::Scissors,
+            _ => panic!("Unexpected choice"),
+        };
 
-        let round_result = get_round_result(opponent_choice, my_choice);
-
-        match round_result {
-            RoundResult::Draw => score += 3,
-            RoundResult::Win => score += 6,
-            _ => (),
-        }
+        score += get_round_score(opponent_choice, my_choice);
     }
 
     println!("{}", score)
 }
 
-pub fn part_2() {}
+pub fn part_2() {
+    let rounds = get_lines_from_file("../inputs/2.txt");
+    let mut score: i32 = 0;
 
-fn get_round_result(opponent_choice: &str, my_choice: &str) -> RoundResult {
-    match (opponent_choice, my_choice) {
-        ("A", "X") => RoundResult::Draw,
-        ("B", "X") => RoundResult::Loss,
-        ("C", "X") => RoundResult::Win,
-        ("A", "Y") => RoundResult::Win,
-        ("B", "Y") => RoundResult::Draw,
-        ("C", "Y") => RoundResult::Loss,
-        ("A", "Z") => RoundResult::Loss,
-        ("B", "Z") => RoundResult::Win,
-        ("C", "Z") => RoundResult::Draw,
-        (_, _) => panic!("Unexpected choices"),
+    for round in rounds {
+        let parsed: Vec<&str> = round.split(' ').collect();
+        let opponent_choice = parse_opponent_play(parsed[0]);
+
+        let desired_result = match parsed[1] {
+            "X" => RoundResult::Loss,
+            "Y" => RoundResult::Draw,
+            "Z" => RoundResult::Win,
+            _ => panic!("Unexpected result"),
+        };
+
+        score += get_round_score_from_desired_result(opponent_choice, desired_result);
     }
+
+    println!("{}", score)
+}
+
+fn get_round_score_from_desired_result(
+    opponent_choice: Choice,
+    desired_result: RoundResult,
+) -> i32 {
+    match (desired_result, opponent_choice) {
+        (RoundResult::Win, Choice::Rock) => 6 + 2,
+        (RoundResult::Win, Choice::Paper) => 6 + 3,
+        (RoundResult::Win, Choice::Scissors) => 6 + 1,
+        (RoundResult::Loss, Choice::Rock) => 0 + 3,
+        (RoundResult::Loss, Choice::Paper) => 0 + 1,
+        (RoundResult::Loss, Choice::Scissors) => 0 + 2,
+        (RoundResult::Draw, Choice::Rock) => 3 + 1,
+        (RoundResult::Draw, Choice::Paper) => 3 + 2,
+        (RoundResult::Draw, Choice::Scissors) => 3 + 3,
+    }
+}
+
+fn get_round_score(opponent_choice: Choice, my_choice: Choice) -> i32 {
+    match (opponent_choice, my_choice) {
+        (Choice::Rock, Choice::Rock) => 3 + 1,
+        (Choice::Paper, Choice::Rock) => 0 + 1,
+        (Choice::Scissors, Choice::Rock) => 6 + 1,
+        (Choice::Rock, Choice::Paper) => 6 + 2,
+        (Choice::Paper, Choice::Paper) => 3 + 2,
+        (Choice::Scissors, Choice::Paper) => 0 + 2,
+        (Choice::Rock, Choice::Scissors) => 0 + 3,
+        (Choice::Paper, Choice::Scissors) => 6 + 3,
+        (Choice::Scissors, Choice::Scissors) => 3 + 3,
+    }
+}
+
+fn parse_opponent_play(opponent_choice: &str) -> Choice {
+    match opponent_choice {
+        "A" => Choice::Rock,
+        "B" => Choice::Paper,
+        "C" => Choice::Scissors,
+        _ => panic!("Unexpected choice"),
+    }
+}
+
+enum Choice {
+    Rock,
+    Paper,
+    Scissors,
 }
 
 enum RoundResult {
