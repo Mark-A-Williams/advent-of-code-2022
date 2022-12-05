e1768e877c0e638412077e18f7d2c56a51e6cd35
Mark Williams <mark.williams@enable.com>
2022-12-05 19:05:41 +0000

    Remove blank entries

--- aoc-2022-rust/src/solutions/day_5.rs
+++ aoc-2022-rust/src/solutions/day_5.rs
@@ -12,11 +12,7 @@ pub fn part_2() {
 }
 
 fn parse_crates() -> Vec<Vec<char>> {
-    let mut crates: Vec<Vec<char>> = vec![];
-
-    for _ in 0..9 {
-        crates.push(vec![]);
-    }
+    let mut crates: Vec<Vec<char>> = (0..9).map(|_| vec![]).collect();
 
     let all_lines = get_lines_from_file("../inputs/5.txt");
     let first_few_lines = all_lines[0..8].iter();
@@ -24,8 +20,10 @@ fn parse_crates() -> Vec<Vec<char>> {
     for line in first_few_lines.rev() {
         let character_enumerator = line.chars().skip(1).step_by(4).enumerate();
         for (index, character) in character_enumerator {
-            let relevant_crate = &mut crates[index];
-            relevant_crate.push(character);
+            if character.is_ascii_uppercase() {
+                let relevant_crate = &mut crates[index];
+                relevant_crate.push(character);
+            }
         }
     }
 
