9f755568b457a324046356d0846683563507a272
Mark Williams <mark.williams@enable.com>
2022-12-05 19:04:01 +0000

    Parsing logic

---
+++ aoc-2022-rust/src/solutions/day_5.rs
@@ -0,0 +1,33 @@
+use super::file_helpers::get_lines_from_file;
+
+pub fn part_1() {
+    let crates = parse_crates();
+    for ccrate in crates.iter() {
+        println!("{:?}", ccrate);
+    }
+}
+
+pub fn part_2() {
+    todo!()
+}
+
+fn parse_crates() -> Vec<Vec<char>> {
+    let mut crates: Vec<Vec<char>> = vec![];
+
+    for _ in 0..9 {
+        crates.push(vec![]);
+    }
+
+    let all_lines = get_lines_from_file("../inputs/5.txt");
+    let first_few_lines = all_lines[0..8].iter();
+
+    for line in first_few_lines.rev() {
+        let character_enumerator = line.chars().skip(1).step_by(4).enumerate();
+        for (index, character) in character_enumerator {
+            let relevant_crate = &mut crates[index];
+            relevant_crate.push(character);
+        }
+    }
+
+    crates
+}
